9929544312a28c6cc322b1929d3d2cc63fcb5cb8
Yuichiro Sawai <sawai.yuichiro.sn0@is.naist.jp>
2018-12-05 22:23:31 +0900

    Extract definition from English wiktionary

--- scripts/english_wiktionary.py
+++ scripts/english_wiktionary.py
@@ -1,18 +1,51 @@
 import json
 import sys
 
+POS = {
+    'noun': 'n.',
+    'adjective': 'adj.',
+    'verb': 'v.',
+    'proper-noun': 'n.',
+    'adverb': 'adv.',
+    'preposition': 'prep.',
+    'initialism': 'abbr.',
+    'interjection': 'interj.',
+    'prepositional-phrase': 'idiom',
+    'prefix': 'pref.',
+    'phrase': 'idiom',
+    'abbreviation': 'abbr.',
+    'proverb': 'idiom',
+    'suffix': 'suff.',
+    'contraction': 'contraction',
+    'pronoun': 'pron.',
+    'numeral': 'numeral',
+    'conjunction': 'conj.',
+    'determiner': 'determiner',
+    'number': 'number',
+    'particle': 'particle',
+}
+
 
 def main():
     path = sys.argv[1]
     words = []
     with open(path) as f:
         for line in f:
-            obj = json.loads(line)
+            obj = json.loads(line)[0]
             word = obj[0]
-            words.append(word)
+            pos = obj[1]
+            variants = obj[3]
+            definition = obj[4]
+            words.append((word, pos, variants, definition))
     words.sort()
-    for word in words:
-        print(json.dumps({'word': word}))
+    for word, pos, variants, definition in words:
+        pos = POS[pos]
+        obj = {
+            'word': word,
+            'lemmas': [f'[{pos}] {word}'],
+            'defs': [definition],
+        }
+        print(json.dumps(obj))
 
 
 if __name__ == '__main__':
