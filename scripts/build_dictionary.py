d9202783885cc0c86b89fe8452972a965c4d89c2
Yuichiro Sawai <sawai.yuichiro.sn0@is.naist.jp>
2018-12-10 00:21:04 +0900

    Split dictionary into chunks of smaller dictionaries

--- scripts/build_dictionary.py
+++ scripts/build_dictionary.py
@@ -1,26 +1,29 @@
 import json
 import sys
+import os
 from collections import defaultdict
 
 from util import lemmatize, load_dictionary
 
 
-def main(lang, dict_path, lemmatizer_path, frequency_path):
+def main(args):
+    # load word frequency data
     freq_dict = defaultdict(int)
-    entries = {}
-    with open(frequency_path) as f:
+    with open(args.frequency_path) as f:
         for line in f:
             form, freq = line.strip().split()
             freq = int(freq)
-            lemma = lemmatize(form, lang, lemmatizer_path, lower=True)
+            lemma = lemmatize(form, args.lang, args.lemmatizer_path, lower=not args.no_lower)
             key = ' '.join(lemma)
             freq_dict[key] += freq
 
-    d = load_dictionary(dict_path)
+    # load dictionary data
+    entries = {}
+    d = load_dictionary(args.dict_path)
     for word, obj in d.items():
-        lemmas = lemmatize(word, lang, lemmatizer_path)
+        lemmas = lemmatize(word, args.lang, args.lemmatizer_path)
         key = ' '.join(lemmas)
-        freq_key = ' '.join(lemmatize(word, lang, lemmatizer_path, lower=True))
+        freq_key = ' '.join(lemmatize(word, args.lang, args.lemmatizer_path, lower=not args.no_lower))
         entry = {
             'word': word,
             'freq': freq_dict[freq_key],
@@ -30,8 +33,35 @@ def main(lang, dict_path, lemmatizer_path, frequency_path):
         if 'lemmas' in obj:
             entry['lemmas'] = obj['lemmas']
         entries[key] = entry
-    print(json.dumps(entries))
+    
+    # split entries into chunks
+    index = {}
+    chunks = defaultdict(dict)
+    for i, (key, entry) in enumerate(sorted(entries.items(), key=lambda kv: -kv[1]['freq'])):
+        chunk_index = i // args.size
+        index[key] = chunk_index
+        chunks[chunk_index][key] = entry
+
+    # write to files
+    index_path = os.path.join(args.out, 'index')
+    os.makedirs(args.out)
+    with open(index_path, 'w') as f_index:
+        json.dump(index, f_index)
+    for i, chunk in chunks.items():
+        print(f'Writing chunk {i+1}/{len(chunks)} ...', file=sys.stderr)
+        chunk_path = os.path.join(args.out, 'entries' + str(i))
+        with open(chunk_path, 'w') as f_chunk:
+            json.dump(chunk, f_chunk)
 
 
 if __name__ == '__main__':
-    main(*sys.argv[1:])
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('lang')
+    parser.add_argument('dict_path')
+    parser.add_argument('lemmatizer_path')
+    parser.add_argument('frequency_path')
+    parser.add_argument('out')
+    parser.add_argument('--size', type=int, default=5000)
+    parser.add_argument('--no-lower', action='store_true', help='don\'t lowercase words when looking up frequency')
+    main(parser.parse_args())
--- scripts/build_files.sh
+++ scripts/build_files.sh
@@ -3,6 +3,6 @@ for lang in German English Chinese ChineseHanzi; do
     python scripts/build_lemmatizer.py $lang data/$lang/{dict,inflection} > app/data/$lang/lemmatizer.json
     python scripts/build_trie.py $lang data/$lang/dict app/data/$lang/lemmatizer.json > app/data/$lang/trie.json
     python scripts/build_entries.py $lang data/$lang/dict app/data/$lang/lemmatizer.json > app/data/$lang/entries.json
-    python scripts/build_dictionary.py $lang data/$lang/dict app/data/$lang/lemmatizer.json data/$lang/frequency > app/data/$lang/dictionary.json
+    python scripts/build_dictionary.py $lang data/$lang/dict app/data/$lang/lemmatizer.json data/$lang/frequency app/data/$lang/dictionary
   )
 done
