e1dfb09f66af8aaf739e176ac11cfd1d6c816ba1
Yuichiro Sawai <sawai.yuichiro.sn0@is.naist.jp>
2019-02-10 23:00:31 +0900

    Add unsafe-eval CSP for production

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,7 +2,11 @@ const merge = require('webpack-merge');
 
 const {
   config,
+  manifest
 } = require('./webpack.common.js');
+
+// currently this is necessary because v-html is used to display dictionary info in the word list
+manifest.content_security_policy = "script-src 'self' 'unsafe-eval'; object-src 'self'"
 
 module.exports = merge(config, {
   mode: 'production',
